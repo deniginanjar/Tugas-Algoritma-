53980122e0d7898954da066da77b9cc2ec245653
deniginanjar <deginsundanese@gmail.com>
2017-03-15 20:55:17 +0700

    Add files via upload

---
+++ Tugas 1 - Deni Ginanjar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "Hello World" << endl ;
+	cout << "I Love Living In This World" << endl;
+	cout << "Deni Ginanjar" << endl;
+	cout << " 16 111 184" << endl;
+	cout << "TIF 16 B Malam" << endl;
+	return 0;
+	
+}
