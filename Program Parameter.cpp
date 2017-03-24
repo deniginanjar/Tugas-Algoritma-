9de49a7d15802b3f1a3cc263bbac92e24c918df2
deniginanjar <deginsundanese@gmail.com>
2017-03-24 23:10:39 +0700

    Add files via upload

---
+++ Program Parameter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int tambahkan(int a, int b);
+
+int main () {
+	cout << "hasil : " << tambahkan (5, 6);
+return 0;
+}
+
+int tambahkan (int a, int b) {
+
+return a + b;
+} 
---
+++ Program Tambah Kurang.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void prosesBaca();
+int prosesTambah();
+int prosesKurang();
+void prosesTampil();
+
+	int a,b;
+	
+int main () {
+	
+	prosesBaca();
+	prosesTampil();
+	return 0;
+}
+
+void prosesBaca () {
+	
+	cout << "Masukan Nilai A : ";
+	cin >> a;
+	cout << "Masukan Nilai B : ";
+	cin >> b;
+}
+
+int prosesTambah () {
+	
+	return a+b;
+}
+
+int prosesKurang () {
+	int hasil;
+	hasil = a-b;
+	
+	return hasil;	
+}
+
+void prosesTampil() {
+	
+	cout << "Hasil Tambah : " << prosesTambah() << endl;
+	cout << "Hasil Kurang : " << prosesKurang();
+}
